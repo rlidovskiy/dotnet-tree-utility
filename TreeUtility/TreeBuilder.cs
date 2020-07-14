26422195a743c73a25a4704cb44d8078bb5950b4
utmx <utmx123@gmail.com>
2020-07-14 17:56:14 +0700

    minor: recursive / non-recursive

--- TreeUtility/TreeBuilder.cs
+++ TreeUtility/TreeBuilder.cs
@@ -16,6 +16,67 @@ namespace TreeUtility
         {
             //put your solution here
             //_output.Write(...);
+
+
+        }
+
+
+
+        void DirSearch(string sDir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(sDir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        lstFilesFound.Items.Add(f);
+                    }
+                    DirSearch(d);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                _output.WriteLine(ex.Message);
+            }
+        }
+
+
+        static IEnumerable<string> GetFiles(string path)
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue(path);
+            while (queue.Count > 0)
+            {
+                path = queue.Dequeue();
+                try
+                {
+                    foreach (string subDir in Directory.GetDirectories(path))
+                    {
+                        queue.Enqueue(subDir);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+                string[] files = null;
+                try
+                {
+                    files = Directory.GetFiles(path);
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+                if (files != null)
+                {
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        yield return files[i];
+                    }
+                }
+            }
         }
     }
 }
