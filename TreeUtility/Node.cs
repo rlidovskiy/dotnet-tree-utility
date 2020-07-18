72200754787730e6ca85e3097b84a48c34d9340c
utmx <utmx123@gmail.com>
2020-07-18 23:50:07 +0700

    review: Node struct added

---
+++ TreeUtility/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TreeUtility
+{
+    public struct Node
+    {
+        public Node(string path)
+        {
+            Path = path;
+            Name = System.IO.Path.GetFileName(path);
+        }
+
+        public string Path { get; }
+        public string Name { get; }
+
+    }
+}
